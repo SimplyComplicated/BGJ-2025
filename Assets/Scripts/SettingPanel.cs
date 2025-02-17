921c6464a5f71598fd16f138c3cbb655e4fa928c
Mohit Gulecha <mohitgulecha2001@gmail.com>
2025-02-17 19:00:58 +0530

    Completed SFX including both Coding and UI

--- Assets/Scripts/SettingPanel.cs
+++ Assets/Scripts/SettingPanel.cs
@@ -7,13 +7,26 @@ public class SettingPanel : MonoBehaviour
     public TextMeshProUGUI volumeTextIndicator;
     public Slider volumeSlider;
     public string parameterName;
-    
+    void Start()
+    {
+        volumeSlider.value=GetCurrentVolume();
+        float volume=volumeSlider.value;
+        volumeTextIndicator.text=((int)(volume*100)).ToString();
+    }
+
     public void OnVolumeChange()
     {
         float volume=volumeSlider.value;
         volumeTextIndicator.text=((int)(volume*100)).ToString();// volumeSlider.value.ToString();
         float dbValue=volume>0? Mathf.Log10(volume) * 20 : -80f;
         AudioManager.Instance.mainMixer.SetFloat(parameterName, dbValue);
+    }
+
+    public float GetCurrentVolume()
+    {
+        float volumeValue;
+        AudioManager.Instance.mainMixer.GetFloat(parameterName, out volumeValue);
+        return Mathf.Pow(10, volumeValue / 20);
     }
     // private void SetVolume(string parameterName, float value)
     // {
